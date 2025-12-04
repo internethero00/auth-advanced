5220c78b17e18ca6d65b736cb81cad53c28250ad
internethero00 <egormix1703@gmail.com>
2025-12-04 22:29:42 +0200

    nest initialization

---
+++ server-side/src/libs/common/utils/is-dev.utils.ts
@@ -0,0 +1,9 @@
+import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
+dotenv.config();
+
+export const isDev = (configService: ConfigService) =>
+  configService.get('NODE_ENV') === 'development';
+
+export const IS_DEV_ENV = process.env.NODE_ENV === 'development';
