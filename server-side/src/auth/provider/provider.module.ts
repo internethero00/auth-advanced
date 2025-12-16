23e36640ca74e4497ed2f3452acd24317de6202b
internethero00 <egormix1703@gmail.com>
2025-12-16 19:20:58 +0200

    + provider auth update

--- server-side/src/auth/provider/provider.module.ts
+++ server-side/src/auth/provider/provider.module.ts
@@ -1,7 +1,39 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import {
+  ProviderOptionSymbol,
+  TypeAsyncOptions,
+  TypeOptions,
+} from './provider.constants';
 import { ProviderService } from './provider.service';
 
-@Module({
-  providers: [ProviderService],
-})
-export class ProviderModule {}
+@Module({})
+export class ProviderModule {
+  public static register(options: TypeOptions): DynamicModule {
+    return {
+      module: ProviderModule,
+      providers: [
+        {
+          useValue: options,
+          provide: ProviderOptionSymbol,
+        },
+        ProviderService,
+      ],
+      exports: [ProviderService],
+    };
+  }
+  public static registerAsync(options: TypeAsyncOptions): DynamicModule {
+    return {
+      module: ProviderModule,
+      imports: options.imports,
+      providers: [
+        {
+          provide: ProviderOptionSymbol,
+          useFactory: options.useFactory,
+          inject: options.inject,
+        },
+        ProviderService,
+      ],
+      exports: [ProviderService],
+    };
+  }
+}
